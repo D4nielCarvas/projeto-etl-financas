6e78085e841d709e4ca53ea1fb37bb4d4b989cf4
Daniel Carvas <danielcarvas28@gmail.com>
2026-03-03 15:37:29 -0300

    feat: Add data transformation script including cleaning, validation, currency conversion, and value categorization.

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pandera.pandas as pa
 
 def transform_data(df: pd.DataFrame, cotacoes: dict) -> pd.DataFrame:
     
@@ -6,12 +7,31 @@ def transform_data(df: pd.DataFrame, cotacoes: dict) -> pd.DataFrame:
     df_transformed = df.drop_duplicates().copy()
     
     # 2. Tratando valores nulos na coluna essencial (valor_brl)
-    df_transformed = df_transformed.dropna(subset=['valor_brl'])
+    df_transformed = df_transformed.dropna(subset=['valor_brl']).copy()
+    
+    # Limpeza e padronização (Ex: removendo espaços extras e capitalizando)
+    if 'produto' in df_transformed.columns:
+        df_transformed['produto'] = df_transformed['produto'].str.strip().str.title()
+        
+    if 'data_venda' in df_transformed.columns:
+        df_transformed['data_venda'] = pd.to_datetime(df_transformed['data_venda'])
     
     # Resetando index para ter uma tabela mais limpa
     df_transformed.reset_index(drop=True, inplace=True)
     
-    # 3. Conversão de moedas
+    # 3. VALIDAÇÃO DE QUALIDADE DE DADOS (PANDERA)
+    # Define o contrato de dados: o que esperamos que o DataFrame contenha exatamente
+    schema_vendas = pa.DataFrameSchema({
+        "id_transacao": pa.Column(pa.Int, coerce=True),
+        "data_venda": pa.Column(pa.DateTime, coerce=True),
+        "produto": pa.Column(pa.String, coerce=True),
+        "valor_brl": pa.Column(pa.Float, checks=pa.Check.ge(0), coerce=True) # Não pode haver vendas negativas
+    })
+    
+    # Executa a validação rigorosa (Quebra o pipeline se os dados forem inválidos)
+    df_transformed = schema_vendas.validate(df_transformed)
+    
+    # 4. Conversão de moedas
     usd_rate = cotacoes.get('USD')
     eur_rate = cotacoes.get('EUR')
     
